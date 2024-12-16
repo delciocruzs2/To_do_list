afd33f117ad2635d6c1f0546b769289089bb6191
Delcio Cruz <delciocruzoficial@gmail.com>
2024-12-15 23:35:50 -0300

    feat(screen): Frames header, nav e main adicionados

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import customtkinter
+import customtkinter as ctk
 import sys
 
-class Application(customtkinter.CTk):
+class Application(ctk.CTk):
     
     def __init__(self) -> object:
-        super().__init__()
-        self.__root = customtkinter.CTk()
+        self.__root = ctk.CTk()
         self._FullscreenSettings()
+        self.header()
+        self.nav()
+        self.main()
         self.__root.mainloop()
 
 
@@ -16,7 +18,17 @@ class Application(customtkinter.CTk):
         self.__root.resizable(False,False)
 
 
+    def header(self) -> object:
+        self.frame_header = ctk.CTkFrame(self.__root, border_width=1)
+        self.frame_header.place(relx=0, rely=0, relwidth=1, relheight=0.2)
+    
+    def nav(self) -> object:
+        self.frame_nav = ctk.CTkFrame(self.__root, border_width=1)
+        self.frame_nav.place(relx=0, rely=0.2, relwidth=0.2, relheight=0.8)
 
+    def main(self) -> object:
+        self.frame_main = ctk.CTkFrame(self.__root, border_width=1)
+        self.frame_main.place(relx=0.2, rely=0.2, relwidth=0.8, relheight=0.8)
 
 
 
