63082fbd54b03f2254487256cec4be37d39b1575
Delcio Cruz <delciocruzoficial@gmail.com>
2024-12-16 19:57:57 -0300

    feat(Header): adicionado texto e imagem no header

--- main.py
+++ main.py
@@ -24,10 +24,19 @@ class Application(ctk.CTk):
 
     def colors(self) -> object:
         self.lavender = '#E6E6FA'
+        self.royalBlue = '#4169E1'
 
     def header(self) -> object:
-        self.frame_header = ctk.CTkFrame(self.__root, border_width=1, fg_color= self.lavender)
+        # MAIN FRAME
+        self.frame_header = ctk.CTkFrame(self.__root, fg_color= self.royalBlue)
         self.frame_header.place(relx=0, rely=0, relwidth=1, relheight=0.13)
+        # PROCESSING IMAGE
+        image_logo = ctk.CTkImage(light_image=Image.open('./image/logo_tarefas.png'),dark_image=Image.open('./image/logo_tarefas.png'), size=(70,50))
+        image = ctk.CTkLabel(self.frame_header, image=image_logo, text= None)
+        image.place(relx=0, rely=0.05, relwidth=0.2)
+        # MAIN TEXT
+        sentense = ctk.CTkLabel(self.frame_header, text='Sistema de gerenciador de tarefas', font=('arial', 30))
+        sentense.place(relx=0.14, rely=0.1, relwidth=0.52, relheight=0.7)
     
     def nav(self) -> object:
         self.frame_nav = ctk.CTkFrame(self.__root, border_width=1, fg_color= self.lavender)
