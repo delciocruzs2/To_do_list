4942831a3a52237125aedd24bf395e27cce13c35
Delcio Cruz <delciocruzoficial@gmail.com>
2024-12-17 14:18:22 -0300

    feat(screen): Adionados botoes e frames de funcionalides CRUD

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ class Application(ctk.CTk):
     def colors(self) -> object:
         self.lavender = '#E6E6FA'
         self.royalBlue = '#4169E1'
+        self.darkSlateGray = '#2F4F4F'
 
     def header(self) -> object:
         # MAIN FRAME
@@ -39,23 +40,64 @@ class Application(ctk.CTk):
         sentense.place(relx=0.14, rely=0.1, relwidth=0.52, relheight=0.7)
     
     def nav(self) -> object:
-        self.frame_nav = ctk.CTkFrame(self.__root, border_width=1, fg_color= self.lavender)
+        self.frame_nav = ctk.CTkFrame(self.__root, fg_color= self.lavender)
         self.frame_nav.place(relx=0, rely=0.13, relwidth=0.25, relheight=0.87)
 
+        self.button_all_ask = ctk.CTkButton(self.frame_nav, text='Mostrar todas tarefas', command=self.all_ask)
+        self.button_all_ask.place(relx=0, rely=0, relwidth=1, relheight=0.2)
+
+        self.button_new_ask = ctk.CTkButton(self.frame_nav, text='Nova tarefas', command=self.new_ask)
+        self.button_new_ask.place(relx=0, rely=0.2, relwidth=1, relheight=0.2)
+
+        self.button_update_ask = ctk.CTkButton(self.frame_nav, text='Atualizar tarefas', command=self.update_ask)
+        self.button_update_ask.place(relx=0, rely=0.4, relwidth=1, relheight=0.2)
+
+        self.button_delete_ask = ctk.CTkButton(self.frame_nav, text='Excluir tarefas', command=self.delete_ask)
+        self.button_delete_ask.place(relx=0, rely=0.6, relwidth=1, relheight=0.2)
+
+        self.button_about = ctk.CTkButton(self.frame_nav, text='About', command=self.about)
+        self.button_about.place(relx=0, rely=0.8, relwidth=1, relheight=0.2)
+
+    def all_ask(self) -> object:
+        self.frame_all_ask = ctk.CTkFrame(self.frame_main, fg_color=self.lavender, border_width=3)
+        self.frame_all_ask.place(relx=0, rely=0, relwidth=1, relheight=1)
+        # text apresentation
+        text_base = ctk.CTkLabel(self.frame_all_ask, text='Todas as tarefas', fg_color=self.darkSlateGray)
+        text_base.place(relx=0, rely=0, relwidth=1, relheight=0.09)
+
+    def new_ask(self) -> object:
+        self.frame_new_ask = ctk.CTkFrame(self.frame_main, fg_color=self.lavender, border_width=3)
+        self.frame_new_ask.place(relx=0, rely=0, relwidth=1, relheight=1)
+        # text apresentation
+        text_base = ctk.CTkLabel(self.frame_new_ask, text='Adicionar nova tarefa', fg_color=self.darkSlateGray)
+        text_base.place(relx=0, rely=0, relwidth=1, relheight=0.09)
+    
+    def update_ask(self) -> object:
+        self.frame_update_ask = ctk.CTkFrame(self.frame_main, fg_color=self.lavender, border_width=3)
+        self.frame_update_ask.place(relx=0, rely=0, relwidth=1, relheight=1)
+        # text apresentation
+        text_base = ctk.CTkLabel(self.frame_update_ask, text='Atualizar tarefas', fg_color=self.darkSlateGray)
+        text_base.place(relx=0, rely=0, relwidth=1, relheight=0.09)
+    
+    def delete_ask(self) -> object:
+        self.frame_delete_ask = ctk.CTkFrame(self.frame_main, fg_color=self.lavender, border_width=3)
+        self.frame_delete_ask.place(relx=0, rely=0, relwidth=1, relheight=1)
+        # text apresentation
+        text_base = ctk.CTkLabel(self.frame_delete_ask, text='Deletar tarefas', fg_color=self.darkSlateGray)
+        text_base.place(relx=0, rely=0, relwidth=1, relheight=0.09)
+    
+    def about(self) -> object:
+        self.frame_about = ctk.CTkFrame(self.frame_main, fg_color=self.lavender, border_width=3)
+        self.frame_about.place(relx=0, rely=0, relwidth=1, relheight=1)
+        # text apresentation
+        text_base = ctk.CTkLabel(self.frame_about, text='About', fg_color=self.darkSlateGray)
+        text_base.place(relx=0, rely=0, relwidth=1, relheight=0.09)
+
     def main(self) -> object:
-        self.frame_main = ctk.CTkFrame(self.__root, border_width=1, fg_color= self.lavender)
+        self.frame_main = ctk.CTkFrame(self.__root, border_width=3, fg_color= self.lavender)
         self.frame_main.place(relx=0.25, rely=0.13, relwidth=0.75, relheight=0.87)
 
-
-
-
-
-
-
-
-
-
-
+    
 
 
 if __name__ == '__main__':
