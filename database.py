f48e3dc05c3de955299e808ebab42d04946fbab7
Delcio Cruz <delciocruzoficial@gmail.com>
2024-12-18 20:13:20 -0300

    feat(Database): Adicionado add_data, e atualizado a abertura e o fechamento da conexão do banco de dados

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+class DataBaseError(Exception):
+    pass
+
 
 class _DataBase:
 
@@ -9,7 +12,7 @@ class _DataBase:
             self.__cursor = self.__db.cursor() 
         except sqlite3.Error as e:
             self.__db = None
-            raise(f'Error ao conectar o banco de dados: {e}')
+            raise DataBaseError(f'Error ao conectar o banco de dados: {e}')
 
     def create_tables(self) -> object:
         try:
@@ -20,23 +23,34 @@ class _DataBase:
                                 )""")
             self.__db.commit()
         except sqlite3.Error as e:
-            raise(f'Error creating the table in the database: {e}')
+            raise DataBaseError(f'Error creating the table in the database: {e}')
     
     def close_db(self) -> None:
         if self.__db:
             try:
                 self.__db.close()
             except sqlite3.Error as e:
-                raise(f'"Error closing the connection to the database: {e}')
+                raise DataBaseError(f'"Error closing the connection to the database: {e}')
+    
+    def add_data(self, title:str, description:str):
+        try:
+            self.__cursor.execute("""
+            INSERT INTO task(title, description)
+            VALUES(?,?)""",(title,description))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            raise DataBaseError(f'Error adding data: {e}')
 
+    def __enter__(self) -> object:
+        return self
+    
+    def __exit__(self, exc_type,exc_value, traceback):
+        self.close_db()
 
 if __name__ == '__main__':
     try:
-        db = _DataBase()
-        db.create_tables()
-    except sqlite3.Error:
-        pass
-    finally:
-        db.close_db()
-    
+        with _DataBase() as db:
+            db.create_tables()
+    except RuntimeError as e:
+        raise DataBaseError(f'Error in database execution: {e}')
 
